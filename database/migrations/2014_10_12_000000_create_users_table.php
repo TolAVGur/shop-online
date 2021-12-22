66d3dd2038e0820771b94faa697f93b592677ed8
Anatolii <gurov.anatoliy@gmail.com>
2021-12-22 19:19:38 +0200

    Adding an additional field u_type(user type) to define the user's role in the users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('u_type')->default('USR')->comment('ADM-admin, USR-user');
             $table->timestamps();
         });
     }
