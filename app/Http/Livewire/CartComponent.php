8db5772308f26d36749adafe970a35eb67c93b7d
Anatolii <gurov.anatoliy@gmail.com>
2022-01-06 03:44:14 +0200

    adding to the Cart page by clicking the Add to Cart button

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,22 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId) {
+        $product = Cart::instance('cart')->get($rowId);
+        $qty = $product->qty + 1;
+        Cart::instance('cart')->update($rowId, $qty);
+    }
+
+    public function decreaseQuantity($rowId) {
+        $product = Cart::instance('cart')->get($rowId);
+        $qty = $product->qty - 1;
+        Cart::instance('cart')->update($rowId, $qty);
+    }
+
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeComponent::class );
 Route::get('/about', AboutComponent::class );
-Route::get('/cart', CartComponent::class );
+Route::get('/cart', CartComponent::class )->name('product.cart');
 Route::get('/checkout', CheckoutComponent::class );
 Route::get('/contact', ContactComponent::class );
 Route::get('/shop', ShopComponent::class );
